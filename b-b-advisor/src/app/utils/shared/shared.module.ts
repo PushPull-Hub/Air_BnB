38f21a1951b5d0d6476afd41b5821ce8aaa64611
Hamza <dahhani.hamza1998@gmail.com>
2021-02-25 17:57:52 +0100

    create a shared module

---
+++ b-b-advisor/src/app/utils/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SkeletonTextComponent } from './skeleton-text/skeleton-text.component';
+
+@NgModule({
+  declarations: [SkeletonTextComponent],
+  imports: [CommonModule],
+  exports: [SkeletonTextComponent],
+})
+export class SharedModule {}
