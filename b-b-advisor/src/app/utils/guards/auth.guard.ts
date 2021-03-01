3eae8141fdf885c6a0138c9e19d76fff75465eee
Hamza <dahhani.hamza1998@gmail.com>
2021-03-01 17:56:59 +0100

    create a dummy guard implementing canLoad method

---
+++ b-b-advisor/src/app/utils/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  CanLoad,
+  Router,
+  Route,
+  UrlSegment,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanLoad {
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.userIsAuthenticated) {
+      this.router.navigateByUrl('/authentication');
+    }
+    return this.authService.userIsAuthenticated;
+  }
+}
