b6c3bf61f99a133e827447c121e9f0a41beae9fc
Hamza <dahhani.hamza1998@gmail.com>
2021-03-03 16:29:01 +0100

    add an edit method & close the sliding item while navigating to new path

--- b-b-advisor/src/app/places/offers/offers.page.ts
+++ b-b-advisor/src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { Place } from 'src/app/utils/models/Place.model';
 import { PlaceService } from 'src/app/utils/services/place.service';
@@ -12,12 +14,18 @@ export class OffersPage implements OnInit, OnDestroy {
   offers: Place[];
   fetchOffersPLacesSubscription: Subscription;
 
-  constructor(private placeService: PlaceService) {}
+  constructor(private placeService: PlaceService, private router: Router) {}
 
   ngOnInit() {
     this.fetchOffersPLacesSubscription = this.placeService.places.subscribe(
       (places: Place[]) => (this.offers = places)
     );
+  }
+
+  editOffer(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'offers', 'edit', offerId]);
+    console.log('Editing item', offerId);
   }
 
   ngOnDestroy(): void {
