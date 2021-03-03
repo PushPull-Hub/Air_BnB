542464df582230146e572a82fad514f48e7235e5
Hamza <dahhani.hamza1998@gmail.com>
2021-03-03 16:19:11 +0100

    add a dummy dates to each place

--- b-b-advisor/src/app/utils/services/place.service.ts
+++ b-b-advisor/src/app/utils/services/place.service.ts
@@ -13,63 +13,90 @@ export class PlaceService {
       'Manhattan Mansion',
       'In the heart of New york city.',
       'assets/images/hotel_1.jpg',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '11',
       "L'Amour Toujours",
       'Romantic place in Paris.',
       'assets/images/hotel_2.jpg',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '12',
       'The Peninsula Chicago',
       'Not your average city trip!',
       'assets/images/hotel_3.jpg',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '13',
       'Marriott.',
       'Start with the customer in mind.',
       'assets/images/hotel_4.jpg',
-      59.99
+      59.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '14',
       'Ritz-Carlton Hotel.',
       'Promise authentic hospitality',
       'assets/images/hotel_5.jpg',
-      300.0
+      300.0,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '15',
       'Waldorf Astoria Hotels',
       'Tower Club Lounge',
       'assets/images/hotel_6.jpg',
-      140.0
+      140.0,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '16',
       'The Luxury Collection',
       'Unrivalled facilites and amenities',
       'assets/images/hotel_7.jpg',
-      70.0
+      70.0,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '17',
       'Malibu',
       'made with Amore <3 ',
       'assets/images/hotel_8.jpg',
-      120.0
+      120.0,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       '18',
       'Breeze Blows',
       'Not your average city trip!',
       'assets/images/hotel_9.jpg',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
   ]);
 
