35cf247043d36e5a7751c36a7f368758073de819
Hamza <dahhani.hamza1998@gmail.com>
2021-03-03 17:52:57 +0100

    add the place image to the edit page and setup the reactive form logic

--- b-b-advisor/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ b-b-advisor/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Place } from 'src/app/utils/models/Place.model';
+import { PlaceService } from 'src/app/utils/services/place.service';
 
 @Component({
   selector: 'app-edit-offer',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
+  form: FormGroup;
+  place: Place;
 
-  constructor() { }
+  constructor(
+    private placeService: PlaceService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.loadPlaceInformations();
   }
 
+  private loadPlaceInformations() {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      if (!paramMap.has('placeId')) {
+        this.router.navigate['places/offers'];
+        return;
+      }
+
+      this.placeService
+        .getPlaceById(paramMap.get('placeId'))
+        .subscribe((place: Place) => {
+          if (place) {
+            this.place = place;
+            this.form = new FormGroup({
+              title: new FormControl(this.place.title, {
+                updateOn: 'blur',
+                validators: [Validators.required],
+              }),
+              description: new FormControl(this.place.description, {
+                updateOn: 'blur',
+                validators: [Validators.required],
+              }),
+            });
+          }
+        });
+    });
+  }
 }
