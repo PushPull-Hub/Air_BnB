55d72ac2161098a59d04cac51afd7d1e17b02fe2
Hamza <dahhani.hamza1998@gmail.com>
2021-03-03 16:20:26 +0100

    subscribe, fetch places data and render the data on template

--- b-b-advisor/src/app/places/offers/offers.page.ts
+++ b-b-advisor/src/app/places/offers/offers.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Place } from 'src/app/utils/models/Place.model';
+import { PlaceService } from 'src/app/utils/services/place.service';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
   styleUrls: ['./offers.page.scss'],
 })
-export class OffersPage implements OnInit {
+export class OffersPage implements OnInit, OnDestroy {
+  offers: Place[];
+  fetchOffersPLacesSubscription: Subscription;
 
-  constructor() { }
+  constructor(private placeService: PlaceService) {}
 
   ngOnInit() {
+    this.fetchOffersPLacesSubscription = this.placeService.places.subscribe(
+      (places: Place[]) => (this.offers = places)
+    );
   }
 
+  ngOnDestroy(): void {
+    this.fetchOffersPLacesSubscription.unsubscribe();
+  }
 }
