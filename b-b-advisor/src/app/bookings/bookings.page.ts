de19bf10dba0b6398c97a33a368cb470268383ef
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-04 16:17:03 +0100

    avoiding memory leaks by unsubscribing from observables

--- b-b-advisor/src/app/bookings/bookings.page.ts
+++ b-b-advisor/src/app/bookings/bookings.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Booking } from '../utils/models/Booking.model';
 import { BookingService } from '../utils/services/booking.service';
 
@@ -7,14 +8,21 @@ import { BookingService } from '../utils/services/booking.service';
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
 })
-export class BookingsPage implements OnInit {
+export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
+  loadBookingPlacesSubscription: Subscription;
 
   constructor(private bookingService: BookingService) {}
 
   ngOnInit() {
-    this.bookingService.bookings.subscribe((bookingsList) => {
-      this.loadedBookings = bookingsList;
-    });
+    this.loadBookingPlacesSubscription = this.bookingService.bookings.subscribe(
+      (bookingsList) => {
+        this.loadedBookings = bookingsList;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.loadBookingPlacesSubscription.unsubscribe();
   }
 }
