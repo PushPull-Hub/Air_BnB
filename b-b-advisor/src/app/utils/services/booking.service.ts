e2d4e070f487badcd39d89b41e9740ca364b937d
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-04 16:16:14 +0100

    create a method to perform booking a place

--- b-b-advisor/src/app/utils/services/booking.service.ts
+++ b-b-advisor/src/app/utils/services/booking.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { delay, take, tap } from 'rxjs/operators';
 import { Booking } from '../models/Booking.model';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +10,40 @@ import { Booking } from '../models/Booking.model';
 export class BookingService {
   private _bookings = new BehaviorSubject<Booking[]>([]);
 
-  constructor() {}
+  constructor(private authenticationService: AuthenticationService) {}
 
   get bookings() {
     return this._bookings.asObservable();
+  }
+
+  addBooking(
+    placeId: string,
+    placeTitle: string,
+    placeImage: string,
+    firstName: string,
+    lastName: string,
+    guestNumber: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    const newBooking = new Booking(
+      Math.random().toString(),
+      placeId,
+      this.authenticationService.userId,
+      placeTitle,
+      placeImage,
+      firstName,
+      lastName,
+      guestNumber,
+      dateFrom,
+      dateTo
+    );
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap((bookings) => {
+        this._bookings.next(bookings.concat(newBooking));
+      })
+    );
   }
 }
