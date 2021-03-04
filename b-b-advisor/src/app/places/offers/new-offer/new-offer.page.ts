a1926c32149718c6ce385c47be68a30041ab0613
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-04 14:58:48 +0100

    give functionality to add offer button

--- b-b-advisor/src/app/places/offers/new-offer/new-offer.page.ts
+++ b-b-advisor/src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { PlaceService } from 'src/app/utils/services/place.service';
 
 @Component({
   selector: 'app-new-offer',
   templateUrl: './new-offer.page.html',
   styleUrls: ['./new-offer.page.scss'],
 })
-export class NewOfferPage implements OnInit {
+export class NewOfferPage implements OnInit, OnDestroy {
+  form: FormGroup;
+  addingPlaceSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private placeService: PlaceService,
+    private router: Router,
+    private loadingController: LoadingController
+  ) {}
 
   ngOnInit() {
+    this.generateTheForm();
   }
 
+  private generateTheForm() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)],
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)],
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  createOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    this.loadingController
+      .create({
+        message: 'Creating a place...',
+      })
+      .then((loadingElement) => {
+        loadingElement.present();
+        this.addingPlaceSubscription = this.placeService
+          .addPlace(
+            this.form.value.title,
+            this.form.value.description,
+            +this.form.value.price,
+            new Date(this.form.value.dateFrom),
+            new Date(this.form.value.dateTo)
+          )
+          .subscribe(() => {
+            loadingElement.dismiss();
+            this.form.reset();
+            this.router.navigate(['/places/offers']);
+          });
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.addingPlaceSubscription) {
+      this.addingPlaceSubscription.unsubscribe;
+    }
+  }
 }
