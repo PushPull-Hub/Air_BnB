ac425d65104f2941ddafb36a70884e35a72cb0a3
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-09 10:14:20 +0100

    add logic to allow openning a modal to book place and store it in the bookings section

--- b-b-advisor/src/app/places/discover/place-detail/place-detail.page.ts
+++ b-b-advisor/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,8 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import {
+  ActionSheetController,
+  LoadingController,
+  ModalController,
+  NavController,
+} from '@ionic/angular';
 import { Subscription } from 'rxjs';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 import { Place } from 'src/app/utils/models/Place.model';
+import { BookingService } from 'src/app/utils/services/booking.service';
 import { PlaceService } from 'src/app/utils/services/place.service';
 
 @Component({
@@ -12,15 +19,22 @@ import { PlaceService } from 'src/app/utils/services/place.service';
 })
 export class PlaceDetailPage implements OnInit, OnDestroy {
   place: Place;
+  isBookable: boolean;
   loadPlaceSubscription: Subscription;
+  bookPlaceSubscription: Subscription;
 
   constructor(
     private placeService: PlaceService,
+    private bookingService: BookingService,
     private navController: NavController,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private actionSheetController: ActionSheetController,
+    private modalController: ModalController,
+    private loadingController: LoadingController
   ) {}
 
   ngOnInit() {
+    this.isBookable = false;
     this.loadPlace();
   }
 
@@ -42,7 +56,72 @@ export class PlaceDetailPage implements OnInit, OnDestroy {
     });
   }
 
+  bookPlace() {
+    this.actionSheetController
+      .create({
+        header: 'choose a booking mode',
+        buttons: [
+          {
+            text: 'Select a Date',
+            handler: () => this.openBookingModal('select'),
+          },
+          {
+            text: 'Random Date',
+            handler: () => this.openBookingModal('random'),
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          },
+        ],
+      })
+      .then((actionSheetElement) => {
+        actionSheetElement.present();
+      });
+  }
+
+  openBookingModal(mode: 'select' | 'random') {
+    this.modalController
+      .create({
+        component: CreateBookingComponent,
+        componentProps: { selectedPlace: this.place, selectedMode: mode },
+      })
+      .then((modalElement) => {
+        modalElement.present();
+        return modalElement.onDidDismiss();
+      })
+      .then((resultData) => {
+        if (resultData.role === 'confirm') {
+          this.loadingController
+            .create({
+              message: 'Booking place ...',
+            })
+            .then((loadingElement) => {
+              loadingElement.present();
+              const data = resultData.data.bookingData;
+              this.bookPlaceSubscription = this.bookingService
+                .addBooking(
+                  this.place.id,
+                  this.place.title,
+                  this.place.imageUrl,
+                  data.firstName,
+                  data.lastName,
+                  data.guestNumber,
+                  data.startDate,
+                  data.endDate
+                )
+                .subscribe(() => {
+                  loadingElement.dismiss();
+                });
+            });
+        }
+      });
+  }
+
   ngOnDestroy() {
     this.loadPlaceSubscription.unsubscribe();
+    if (this.bookPlaceSubscription) {
+      this.bookPlaceSubscription.unsubscribe();
+    }
   }
 }
