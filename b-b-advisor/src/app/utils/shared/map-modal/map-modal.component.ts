b042457a5230c6e067c8bf9d63e42d956e3d14ef
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-10 17:21:52 +0100

    add the google maps sdk logic into a private method and use it on after view init life cycle hook

--- b-b-advisor/src/app/utils/shared/map-modal/map-modal.component.ts
+++ b-b-advisor/src/app/utils/shared/map-modal/map-modal.component.ts
@@ -1,17 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit {
+export class MapModalComponent implements OnInit, AfterViewInit {
   constructor(private modalController: ModalController) {}
 
   ngOnInit() {}
 
-  cancel() {
+  ngAfterViewInit(): void {
+    this._getGoogleMaps()
+      .then((googleMaps) => {
+        console.log(googleMaps);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  private _getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsAPIKey}`;
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google Maps SDK not available.');
+        }
+      };
+    });
+  }
+
+  cancel(): void {
     this.modalController.dismiss();
   }
 }
