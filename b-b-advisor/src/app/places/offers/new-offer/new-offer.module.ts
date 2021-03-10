09db69e9df9cdaa5eea1fff5a507547a220a6e9a
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-10 16:12:03 +0100

    add the shared module class to the new offer module array of imports

--- b-b-advisor/src/app/places/offers/new-offer/new-offer.module.ts
+++ b-b-advisor/src/app/places/offers/new-offer/new-offer.module.ts
@@ -7,11 +7,12 @@ import { IonicModule } from '@ionic/angular';
 import { NewOfferPageRoutingModule } from './new-offer-routing.module';
 
 import { NewOfferPage } from './new-offer.page';
+import { SharedModule } from 'src/app/utils/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    SharedModule,
     ReactiveFormsModule,
     IonicModule,
     NewOfferPageRoutingModule,
